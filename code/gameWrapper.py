9e49048cbf96be4e76cbe1a34968eecc4563d4d2
Niklas Eckhoff <nikyecky@gmail.com>
2019-12-20 18:24:06 +0100

    added actions and costs to gameWrapper

--- code/gameWrapper.py
+++ code/gameWrapper.py
@@ -1,3 +1,8 @@
+#TODO always test if rounds > 0 for do actions, else throw error
+#TODO always check if city is valid, else throw error (?)
+#TODO use hungarian notation (?)
+
+
 def getRound(game):
     return game["round"]
 
@@ -47,3 +52,99 @@ def getAwareness(city):
 
 def getEvents(city):
     return city["events"]
+
+
+def doEndRound():
+    return {"type" : "endRound"}
+
+
+def doPutUnderQuarantine(city, rounds):
+    return {"type" : "putUnderQuarantine", "city" : city, "rounds": rounds}
+
+
+def doCloseAirport(city, rounds):
+    {"type" : "closeAirport", "city" : "<Name einer Stadt: S>", "rounds" : "<Anzahl Runden: R, natürliche Zahl > 0>"}
+
+
+def doCloseConnection(fromCity, toCity, rounds):
+    {"type" : "closeConnection", "fromCity" : fromCity, "toCity" : toCity, "rounds" : rounds}
+
+
+def doDevelopVaccine(pathogen):
+    {"type" : "developVaccine", "pathogen" : pathogen}
+
+
+def doDeployVaccine(pathogen, city):
+    {"type" : "deployVaccine", "pathogen" : pathogen, "city" : city}
+
+
+def doDevelopMedication(pathogen):
+    {"type" : "developMedication", "pathogen" : pathogen}
+
+
+def doDeployMedication(pathogen, city):
+    {"type" : "deployMedication", "pathogen" : pathogen, "city" : city}
+
+
+def doExertInfluence(city):
+    {"type": "exertInfluence", "city" : city}
+
+
+def doCallElections(city):
+    {"type" : "callElections", "city" : city}
+
+
+def doApplyHygienicMeasures(city):
+    {"type" : "applyHygienicMeasures", "city" : city}
+
+
+def doLaunchCampaign(city):
+    {"type" : "launchCampaign", "city" : city}
+
+
+def costEndRound():
+    return 0
+
+
+def costPutUnderQuarantine(rounds):
+    return 10 * rounds + 20
+
+
+def costCloseAirport(rounds):
+    return 5 * rounds + 15
+
+
+def costCloseConnection(rounds):
+    return 3 * rounds + 3
+
+
+def costDevelopVaccine():
+    return 40
+    
+
+def getCostDeployVaccine():
+    return 5
+
+
+def costDevelopMedication():
+    return 20
+
+
+def costDeployMedication():
+    return 10
+
+
+def costExertInfluence():
+    return 3
+
+
+def costCallElections():
+    return 3
+
+
+def costApplyHygienicMeasures():
+    return 3
+
+
+def costLaunchCampaign():
+    return 3
