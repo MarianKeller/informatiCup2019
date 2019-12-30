7db869ca08ddfd703bdaf32c1a1ff66ab2a4573c
MarianKeller <mariankeller@mondix.de>
2019-12-30 17:03:03 +0100

    First functional version of vectorizeState

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -4,16 +4,34 @@ from enum import Enum
 
 climateZones = Enum('climateZones','TROPICAL SUBTROPICAL MODERATE ARCTIC')
 
-def climateZone(latitude):
-    absLat = math.abs(latitude)
+
+def climateZoneIsTropical(latitude):
+    absoluteLatitude = math.abs(latitude)
     if abslat >= 0 or abslat <= 23.5:
-        return climateZones.TROPICAL
-    elif abslat > 23.5 or absLat <= 40:
-        return climateZones.SUBTROPICAL
-    elif abslat > 40 or absLat <= 60:
-        return climateZones.MODERATE
+        return 1
     else:
-        return climateZones.ARCTIC
+        return 0
+
+
+def climateZoneIsSubTropical(latitude):
+    if absoluteLatitude > 23.5 or absoluteLatitude <= 40:
+        return 1
+    else:
+        return 0
+
+
+def climateZoneIsModerate(latitude):
+    if absoluteLatitude > 40 or absoluteLatitude <= 60:
+        return 1
+    else:
+        return 0
+
+
+def climateZoneIsArctic(latitude):
+    if absoluteLatitude > 60 or absoluteLatitude <= 90:
+        return 1
+    else:
+        return 0
 
 
 def getConnectivity(game, city):
@@ -97,12 +115,26 @@ def vectorizeState(gameState):
         cityStateVector.append(points)
 
         # dependent of city non-indicator
-        cityStateVector.append(climateZone(gw.getLatitude(city)))
-        cityStateVector.append(gw.getPopulation(city))
-        cityStateVector.append(gw.getEconomy(city))
-        cityStateVector.append(gw.getGovernment(city))
-        cityStateVector.append(gw.getHygiene(city))
-        cityStateVector.append(gw.getAwareness(city))
-        cityStateVector.append()
+        cityStateVector.append(gw.getPopulation(game, city))
+        cityStateVector.append(gw.getEconomy(game, city))
+        cityStateVector.append(gw.getGovernment(game, city))
+        cityStateVector.append(gw.getHygiene(game, city))
+        cityStateVector.append(gw.getAwareness(game, city))
     
         # dependent of city indicator
+        latitude = gw.getLatitude(game, city)
+        cityStateVector.append(climateZoneIsTropical(latitude))
+        cityStateVector.append(climateZoneIsSubTropical(latitude))
+        cityStateVector.append(climateZoneIsModerate(latitude))
+        cityStateVector.append(climateZoneIsArctic(latitude))
+
+        cityStateVector.append(climateZone(gw.getLatitude(game, city)))
+        cityStateVector.append(getConnectivity(game, city))
+        cityStateVector.append(getHighestPathogenInfectivity(game, city)[1])
+        cityStateVector.append(getHighestPathogenMobility(game, city)[1])
+        cityStateVector.append(getHighestPathogenLethality(game, city)[1])
+        cityStateVector.append(getHighestPathogenPrevalence(game, city)[1])
+
+        # TODO missing indicator values
+        
+    return cityStateVector
