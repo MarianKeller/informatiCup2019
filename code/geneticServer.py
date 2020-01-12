e855d1235e8ad74bcfe34aeb6e6b79e049821614
MarianKeller <mariankeller@mondix.de>
2020-01-12 08:35:38 +0100

    created class

--- code/geneticServer.py
+++ code/geneticServer.py
@@ -1,13 +1,22 @@
 import json
 from bottle import BaseRequest, post, request, run, route
+import requests
 
 geneticServerPort = 50124
 geneticServerUrl = "http://localhost:50124"
 geneticServerIP = "0.0.0.0"
-def launchGeneticServer():
-    gs = gameServer()
-    route("/", "POST", ts.startGameWithGenome)
-    BaseRequest.MEMFILE_MAX = 10 * 1024 * 1024
-    run(host=gameServerIP, port=gameServerPort, quiet=True)
 
-launchTrainingServer()
+
+class geneticServer(object):
+    def launchCallbackServer(self, path):
+        route(path, "POST", ts.startGameWithGenome)
+
+
+    def evaluateGenome(self, genome, genomeId):
+        pass
+
+    def newTrainingCampaign(self):
+        pass
+
+BaseRequest.MEMFILE_MAX = 10 * 1024 * 1024
+run(host=gameServerIP, port=gameServerPort, quiet=True)
