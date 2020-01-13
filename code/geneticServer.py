70bd52f87ca2798f45aa8ed42cfe3d307a7bba3a
Niklas <nikyecky@gmail.com>
2020-01-13 17:42:30 +0100

    organized imports, formatted

--- code/geneticServer.py
+++ code/geneticServer.py
@@ -1,9 +1,11 @@
 import json
-from bottle import BaseRequest, post, request, run, route
-import requests
-from deap import tools, creator, base, algorithms
-import numpy
 from hashlib import blake2s
+
+import numpy
+from bottle import BaseRequest, post, request, route, run
+
+import requests
+from deap import algorithms, base, creator, tools
 
 geneticServerPort = 50122
 geneticServerUrl = "http://localhost:50122"
@@ -11,10 +13,12 @@ geneticServerIP = "0.0.0.0"
 
 trainingServerUrl = "http://localhost:50124"
 
+
 def hashBlake2(val, hSize=32):
-        h = blake2s(digest_size=hSize)
-        h.update(val)
-        return h.hexdigest()
+    h = blake2s(digest_size=hSize)
+    h.update(val)
+    return h.hexdigest()
+
 
 class geneticServer(object):
 
@@ -22,10 +26,8 @@ class geneticServer(object):
         self.genomeFitnessDictionary = {}
         pass
 
-
     def launchCallbackServer(self, path):
         route(path, "POST", self.computeFitness)
-    
 
     def computeFitness(self):
         params = request.json
@@ -33,24 +35,23 @@ class geneticServer(object):
         fitnessVect = []
         for [genomeNr, result, rounds] in params["gameResults"]:
             intResult = int(result == "win")
-            fitnessVect.append([0.5 + ((-1)^(intResult+1))*(1/rounds)*0.5])
+            fitnessVect.append([0.5 + ((-1) ^ (intResult+1))*(1/rounds)*0.5])
         self.genomeFitnessDictionary[genomeId] = numpy.median(fitnessVect)
         self.callbackFunction(genomeId, self.genomeFitnessDictionary[genomeId])
         print(fitnessVect, " median: ", self.genomeFitnessDictionary[genomeId])
         return "ACK"
 
-
     def evaluateGenome(self, genome, callbackFunction):
         self.callbackFunction = callbackFunction
-        genomeId = str(hashBlake2(genome.tostring(),10))
+        genomeId = str(hashBlake2(genome.tostring(), 10))
         self.launchCallbackServer("/genomeperformance/" + genomeId)
 
         genomeRunCount = 2
 
-        postData = {"genomeId" : genomeId, 
-                    "callbackUrl" : geneticServerUrl + "/genomeperformance/" + genomeId,
-                    "runCount" : genomeRunCount,
-                    "genome" : genome.tolist()}
+        postData = {"genomeId": genomeId,
+                    "callbackUrl": geneticServerUrl + "/genomeperformance/" + genomeId,
+                    "runCount": genomeRunCount,
+                    "genome": genome.tolist()}
 
         requests.post(trainingServerUrl + "/startwithgenome", json=postData)
         return genomeId
@@ -58,8 +59,10 @@ class geneticServer(object):
     def newTrainingCampaign(self):
         pass
 
+
 def resultReady(genomeId, medianFitness):
     print("result: ", medianFitness)
+
 
 genServ = geneticServer()
 genServ.evaluateGenome(numpy.random.rand(12, 31), resultReady)
