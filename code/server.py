da8ecca6b351863a6606ea566260d94869bda01b
Niklas Eckhoff <nikyecky@gmail.com>
2020-01-14 04:22:35 +0100

    dynamically adjust game file path based on os

--- code/server.py
+++ code/server.py
@@ -6,8 +6,13 @@ import subprocess
 import postprocessor as actor
 from gameWrapper import GameWrapper
 import preprocessor as pre
+import os
 
-gameFilePath = "ic20_windows"
+if os.name == 'posix':
+    gameFilePath = "ic20/ic20_linux"
+elif os.name == 'nt':
+    gameFilePath = "ic20/ic20_windows"
+
 
 class playerServer(object):
     def __init__(self, id="game1", count=1, trainer=None, genome=None):
@@ -15,23 +20,27 @@ class playerServer(object):
         self.myTrainer = trainer
         self.genomeId = id
         self.genomeCount = count
-        self.genome = genome #TODO: specify genome format, pass from request and actually use the passed genome
-
+        # TODO: specify genome format, pass from request and actually use the passed genome
+        self.genome = genome
 
     def gamePlayer(self):
         gameDict = request.json
         game = GameWrapper(gameDict)
         print(f'round: {game.getRound()}, outcome: {game.getOutcome()}')
         if(game.getOutcome() == 'pending'):
-            action = actor.action(game, self.genome, doManualOptimizations=(not self.hasTrainer))
-            #action = GameWrapper.doEndRound() # TODO: remove this line
-            print("numPossibleActions: ", actor.numPossibleActions, "inputVectorSize: ", pre.inputVectorSize)
+            action = actor.action(
+                game, self.genome, doManualOptimizations=(not self.hasTrainer))
+            # action = GameWrapper.doEndRound() # TODO: remove this line
+            print("numPossibleActions: ", actor.numPossibleActions,
+                  "inputVectorSize: ", pre.inputVectorSize)
             print(self.genomeId, str(self.genomeCount), " action:", action, "\n")
             return action
         else:
             if self.hasTrainer:
-                print(self.genomeId, str(self.genomeCount), " from trainer: ", game.getOutcome(), " round: ", game.getRound())
-                self.myTrainer.collectGameResult(self.genomeCount, game.getOutcome(), game.getRound())
+                print(self.genomeId, str(self.genomeCount), " from trainer: ",
+                      game.getOutcome(), " round: ", game.getRound())
+                self.myTrainer.collectGameResult(
+                    self.genomeCount, game.getOutcome(), game.getRound())
             else:
                 print(game.getOutcome(), " round: ", game.getRound())
         return ""
@@ -44,7 +53,6 @@ class trainingServer(object):
         self.runCount = params["runCount"]
         self.genome = np.array(params["genome"])
 
-
     def startGameWithGenome(self):
         params = request.json
         print(params)
@@ -52,31 +60,32 @@ class trainingServer(object):
 
         self.gameResults = []
 
-        for i in range(0,self.runCount):
+        for i in range(0, self.runCount):
             ps = playerServer(self.genomeId, i, self, self.genome)
             path = "/" + self.genomeId + str(i)
             route(path, "POST", ps.gamePlayer)
-            subprocess.Popen([gameFilePath, "-u", trainingServerUrl + path, 
-                "-o", "logs/log_", self.genomeId, str(i), ".txt"])
+            subprocess.Popen([gameFilePath, "-u", trainingServerUrl + path,
+                              "-o", "logs/log_", self.genomeId, str(i), ".txt"])
             print(self.genomeId, " playing at: ", path)
 
-        return {"id" : self.genomeId, "state" : "started"}
-
+        return {"id": self.genomeId, "state": "started"}
 
     def collectGameResult(self, genomeNr, result, rounds):
         self.gameResults.append([genomeNr, result, rounds])
         if len(self.gameResults) >= self.runCount:
             self.returnGameResults()
 
-
     def returnGameResults(self):
-        jsonGameResults = {"genomeId" : self.genomeId, "gameResults" : self.gameResults}
+        jsonGameResults = {"genomeId": self.genomeId,
+                           "gameResults": self.gameResults}
         requests.post(self.callbackUrl, json=jsonGameResults)
 
 
 trainingServerPort = 50124
 trainingServerUrl = "http://localhost:50124"
 trainingServerIP = "0.0.0.0"
+
+
 def launchTrainingServer():
     ts = trainingServer()
     route("/startwithgenome", "POST", ts.startGameWithGenome)
@@ -87,11 +96,15 @@ def launchTrainingServer():
 gameServerPort = 50123
 gameServerUrl = "http://localhost:50123"
 gameServerIP = "0.0.0.0"
+
+
 def launchGameServer():
-    gs = playerServer(genome=np.random.rand(actor.numPossibleActions, pre.inputVectorSize))
+    gs = playerServer(genome=np.random.rand(
+        actor.numPossibleActions, pre.inputVectorSize))
     route("/", "POST", gs.gamePlayer)
     BaseRequest.MEMFILE_MAX = 10 * 1024 * 1024
     run(host=gameServerIP, port=gameServerPort, quiet=True)
 
-#launchTrainingServer()
+
+# launchTrainingServer()
 launchGameServer()
