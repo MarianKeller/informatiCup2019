7d238353c6644fde456f44f37078be8c4554e5fc
MarianKeller <mariankeller@mondix.de>
2020-01-20 00:21:59 +0100

    deleted start.py

--- code/start.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import requests
-
-from time import sleep
-
-print("")
-subprocess.call(['python', 'genetic.py'], shell=True)
-sleep(5)
-print(requests.post('http://localhost:50122/main', json={}))
